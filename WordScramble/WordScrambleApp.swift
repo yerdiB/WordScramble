70fe87be0536a074b74ece2811d3ab093c173d8d
yerdi <yerdi.isman@gmail.com>
2023-01-07 23:14:46 +0600

    first commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Yerdaulet Ismanaliyev on 07.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
